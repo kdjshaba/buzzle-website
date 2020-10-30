355a301078bfc9b500f5e680be0af02d5173eb97
MassivaM <hamlimassy@hotmail.com>
2020-10-30 16:46:03 -0400

    initial commit , home page , nav bar and front page animation

---
+++ buzzle/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './sass/App.scss';
+import NavBar from './components/NavBar'
+import Intro from './components/Intro'
+function App() {
+  return (
+    <div className="App">
+    <NavBar/>
+    <Intro/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ buzzle/src/components/NavBar.js
@@ -0,0 +1,175 @@
+import React, { Component }  from 'react';
+import PropTypes from 'prop-types';
+import {
+  AppBar, Toolbar, Typography, List, ListItem,
+  withStyles, Grid, SwipeableDrawer
+} from '@material-ui/core';
+
+
+import buzzle from '../assets/images/buzzle.svg'
+
+
+const styleSheet = {
+  root: {
+
+    padding:0,
+    margin:0
+  },
+
+  list : {
+    width : 100,
+  },
+  padding : {
+    paddingRight : 30,
+    cursor : "pointer",
+  },
+
+  sideBarIcon : {
+    padding : 0,
+    color : "white",
+    cursor : "pointer",
+  },
+  logo:{
+    padding:0,
+    margin: 0
+
+  },
+  list:{
+
+
+  },buttons:{
+    marginRight:'auto',
+  
+    color: 'black', 
+    
+   
+  
+  
+  },
+  button:{
+    margin:11,
+    fontSize: 15,
+    
+  
+  
+  },
+  links:{
+     
+    color: '#0049B8', 
+    padding:0,
+    textDecoration : 'none',
+    '&:hover': {
+      color: "#6c5ce7",
+   }
+  },
+  links2:{
+     
+    color: 'black', 
+    padding:0,
+    textDecoration : 'none',
+    '&:hover': {
+      color: '#70a1ff',
+   }
+  },
+   register:{
+    color: 'white',
+    backgroundColor : "#aa4a40",
+    
+    '&:hover': {
+      backgroundColor: "white",
+   }
+  },
+  image:{
+
+   marginLeft: "auto", 
+   marginRight: "auto"
+    
+  }
+
+  
+}
+
+
+//array with nav bar parts
+const navLinks = [
+  {
+      title : 'Home', 
+      path: '/'
+  },
+]
+
+
+class ResAppBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {drawerActivate:false, drawer:false};
+    this.createDrawer = this.createDrawer.bind(this);
+    this.destroyDrawer = this.destroyDrawer.bind(this);
+  }
+
+  componentWillMount(){
+    if(window.innerWidth <= 900){
+      this.setState({drawerActivate:true});
+    }
+
+    window.addEventListener('resize',()=>{
+      if(window.innerWidth <= 900){
+        this.setState({drawerActivate:true});
+      }
+      else{
+        this.setState({drawerActivate:false})
+      }
+    });
+  }
+
+  //Small Screens
+  createDrawer(){
+    const {classes} = this.props
+    return (
+      <div className={styleSheet.root}>
+        <AppBar position="fixed"  style={{ margin: 0, padding: 0, background: '#fffff', boxShadow: 'none',height:60}} >
+          <Toolbar style={{background:'white'}}>
+          
+          </Toolbar>
+        </AppBar>
+
+   
+
+      </div>
+    );
+  }
+
+  //Larger Screens
+  destroyDrawer(){
+    const {classes} = this.props
+    return (
+      <AppBar  style={{flexGrow:0, margin: 0, padding: 0, background: 'white', boxShadow: 'none', height:100}}>
+        <Toolbar  style={{background: 'white', alignContent:'center'}}>
+        <img className={classes.image} src={buzzle}></img>
+            
+
+        </Toolbar>
+      </AppBar>
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.drawerActivate ? this.createDrawer() : this.destroyDrawer()}
+      </div>
+    );
+  }
+}
+
+ResAppBar.propTypes = {
+  classes : PropTypes.object.isRequired
+};
+
+
+
+export default withStyles(styleSheet)(ResAppBar);
+
+
+
+
